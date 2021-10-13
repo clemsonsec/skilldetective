45699cde1a97b9efcb4ebffbbeff8df3cc2322bd
skilldetective <78060243+skilldetective@users.noreply.github.com>
2021-10-13 18:03:12 -0400

    Update QuesTypeAns.java

--- ChatBot/Source/QuesTypeAns.java
+++ ChatBot/Source/QuesTypeAns.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package QandA;
 
 import edu.stanford.nlp.simple.Sentence;
 import edu.stanford.nlp.trees.Tree;
